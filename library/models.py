859ea0c1f4cf429fb585ef9db5d1830096b39c3c
Julian Gamba <gahe2003@gmail.com>
2024-05-15 10:18:19 -0500

    Se crean modelos y superuser

--- library/models.py
+++ library/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthdate = models.DateField()
+    
+    def __str__(self):
+        return self.name + " " + self.last_name
+    
+class Book(models.Model):
+    NOVELA = 'NO'
+    ENSAYO = 'EN'
+    POESIA = 'PO'
+    CIENCIA_FICCION = 'CF'
+
+    GENERO_CHOICES = [
+        (NOVELA, 'Novela'),
+        (ENSAYO, 'Ensayo'),
+        (POESIA, 'Poesía'),
+        (CIENCIA_FICCION, 'Ciencia Ficción'),
+    ]
+
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.PositiveIntegerField()
+    genre = models.CharField(choices=GENERO_CHOICES)
